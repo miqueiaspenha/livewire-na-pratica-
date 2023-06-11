fa00d2a17b28d72d9978a8cfd95f8c985839bacb
Miqueias Penha <miqueiaspenha@gmail.com>
2023-06-11 16:14:21 -0300

    first commit

---
+++ app/Http/Livewire/Expense/ExpenseCreate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Expense;
+
+use Livewire\Component;
+
+class ExpenseCreate extends Component
+{
+    public function render()
+    {
+        return view('livewire.expense.expense-create');
+    }
+}
