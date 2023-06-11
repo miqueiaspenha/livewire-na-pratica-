af015c2ff56e649e47c1e2bf53f8c3df4ec42f22
Miqueias Penha <miqueiaspenha@gmail.com>
2023-06-11 17:42:38 -0300

    M3A15

--- app/Http/Livewire/Expense/ExpenseCreate.php
+++ app/Http/Livewire/Expense/ExpenseCreate.php
@@ -2,10 +2,37 @@
 
 namespace App\Http\Livewire\Expense;
 
+use App\Models\Expense;
 use Livewire\Component;
 
 class ExpenseCreate extends Component
 {
+    public $description;
+    public $amount;
+    public $type;
+
+    protected $rules = [
+        'description' => 'required',
+        'amount' => 'required',
+        'type' => 'required',
+    ];
+
+    public function createExpense()
+    {
+        $this->validate();
+
+        Expense::create([
+            'description' => $this->description,
+            'amount' => $this->amount,
+            'type' => $this->type,
+            'user_id' => 1,
+        ]);
+
+        session()->flash('message', 'Registro criado com sucesso.');
+
+        $this->description = $this->amount = $this->type = null;
+    }
+
     public function render()
     {
         return view('livewire.expense.expense-create');
--- resources/views/livewire/expense/expense-create.blade.php
+++ resources/views/livewire/expense/expense-create.blade.php
@@ -1,3 +1,44 @@
 <div>
-    {{-- Be like water. --}}
+
+    @if (session()->has('message'))
+        <h3>{{ session('message') }}</h3>
+    @endif
+
+    <h3>Criar Registro</h3>
+    <hr>
+
+    <form action="" wire:submit.prevent='createExpense'>
+        <p>
+            <label>Descrição do Registro</label>
+            <input type="text" name="description" class="shadow border-t" wire:model='description'>
+
+            @error('description')
+                {{ $message }}
+            @enderror
+        </p>
+
+        <p>
+            <label>Valor do Registro</label>
+            <input type="text" name="amount" class="shadow border-t" wire:model='amount'>
+
+            @error('amount')
+                {{ $message }}
+            @enderror
+        </p>
+
+        <p>
+            <label>Tipo do Registro</label>
+            <select name="type" class="shadow border-t" wire:model='type'>
+                <option value="">Selecione o tipo do registro</option>
+                <option value="1">Entrada</option>
+                <option value="2">Saída</option>
+            </select>
+
+            @error('type')
+                {{ $message }}
+            @enderror
+        </p>
+
+        <button type="submit" class="shadow border-t">Criar Registro</button>
+    </form>
 </div>
